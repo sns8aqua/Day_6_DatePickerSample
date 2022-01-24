23a9e699d7fb6765ee3d2985bf4d1e569f2b8960
SathyaNarayan <roflmeatu@gmail.com>
2022-01-24 23:37:29 +0100

    Date Picker

--- DatePickerSample/ViewController.swift
+++ DatePickerSample/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var displayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        datePicker.minimumDate = Date()
+        datePicker.maximumDate = Date().addingTimeInterval(10000)
+        
+        
+        
+        let dateString = "05-03-2020 13:56:23"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-YYYY HH:mm:ss"
+        
+        let date = dateFormatter.date(from: dateString)
+        
+        print(date)
+        
+        
+        
     }
 
-
+    @IBAction func dateChanged(_ sender: Any) {
+        print(datePicker.date)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MMMM-YYYY hh:mm:ss a"
+        self.displayLabel.text = dateFormatter.string(from: datePicker.date)
+    }
+    
 }
 
